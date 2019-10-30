8fddc76436c173f7b710c91a8c20db927759eeab
WillySterr <54704862+WillySterr@users.noreply.github.com>
2019-10-30 14:16:54 +0100

    Update deviceManager.class.php

--- class/deviceManager.class.php
+++ class/deviceManager.class.php
@@ -8,4 +8,22 @@
 		public function setDb(PDO $db){
 			$this->_db = $db;
 		}
+
+	public function add(Device $device){
+		//préparation de la requête d'insertion.
+		$add_device = $this->_db->prepare('INSERT INTO device(name) VALUES (:name)');
+		$add_device->bindValue(':name', $device->getName(), PDO::PARAM_STR);
+		// Execution de la requête
+		$add_device->execute();
+		//closeCursor() libère la connexion au serveur, permettant aisni à d'autres requêtes SQL d'être exectuées
+		$add_device->closeCursor();
+		echo '<p>Device Added</p>';
+
+	}
+
+
+
+
+
+
 	}
