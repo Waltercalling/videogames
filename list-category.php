a9a9704b475d293d8695ec14fd93b410ba55c77e
Waltercalling <waltercalling@free.fr>
2019-10-30 19:01:40 +0100

    Update list-category.php

--- list-category.php
+++ list-category.php
@@ -17,22 +17,22 @@
 	<?php $manager = new categoryManager($bdd); 
           $listCategory = $manager->listCategory();
           // $categorydel = $manager->getObjCategory();
-        
+
           // echo'<pre>';
           // var_dump($categorydel);
 	?>
-<table class="w-75 m-auto">
-		<thead class="bg-dark text-white">
-			<th class="p-3">Id</th>
-			<th class="p-3">Catégorie</th>
-			<th class="p-3">Editer</th>
+<table class="w-50 m-auto table">
+		<thead class="thead-dark text-white">
+			<th scope="col" class="p-3">Id</th>
+			<th scope="col" class="p-3 w-100">Catégorie</th>
+			<th scope="col" class="p-3 text-center w-25">Editer</th>
 
 		</thead>
 
 		<tbody>
 			<?php foreach ($listCategory as $key => $value): ?>
 			<tr>
-				<td class="p-3"><?= $value['id_category']; ?></td>
+				<td class="p-3" scope="row"><?= $value['id_category']; ?></td>
 				<td class="p-3"><?= $value['type']; ?></td>
 				<td class="text-right align-middle">
 					<div class="d-flex flex-lg-row justify-content-lg-around">
