63a37e780ed8773bb996a5aea237e14b71098016
waltercalling <waltercalling@free.fr>
2019-10-30 13:34:58 +0100

    update

--- add-category.php
+++ add-category.php
@@ -14,7 +14,25 @@
 
 	<h1 class="text-center my-3">Ajouter une catégorie</h1>
 
+<section class="w-50 m-auto">
+	<form class="border border-dark rounded bg-light p-5" action="" method="POST">
+		
+		<label for="catName">Nom de la catégorie</label>
+		<input type="text" name="catName" class="form-control" />
 
+		<div class="row">
+			<!-- Cancel Button -->
+			<div class="col-12">
+				<a title="Annuler" href="index.php" type="button" name="back" class="btn btn-danger shadow-sm border border-dark w-100"/>Annuler</a>
+			</div>
+			<!-- Validate Button -->
+			<div class="col-12">
+				<input type="submit" name="sendCat" value="Enregistrer la catégorie" class="btn btn-success shadow-sm border border-dark w-100" title="Enregistrer la catégorie"/>
+			</div>	
+		</div>
+
+	</form>
+</section>
 	<!-- Footer -->
 	<?php include("inc/footer.php"); ?>
 
--- inc/head.php
+++ inc/head.php
@@ -10,7 +10,7 @@
 		<link rel="stylesheet" type="text/css" href="css/reset.css">
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
 		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/mdb.min.css">
+<!-- 		<link rel="stylesheet" href="css/mdb.min.css"> -->
 			
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
