18c81d2986656e51a81a4a59206791ceb6a1cc60
WillySterr <54704862+WillySterr@users.noreply.github.com>
2019-10-30 12:54:21 +0100

    Update device.php

--- device.php
+++ device.php
@@ -1,5 +1,17 @@
 <?php
+	
+	spl_autoload_register(function($classe){
+			require_once 'classes/'.$classe.'.class.php';
+			
+		});
+
+	include_once 'inc/connect.php';
+
+	$device = new Device ([
+		'name' => 'Playstation 4',
+	]);
 
 
+	$manager = new GuitareManager($bdd);
 
 ?>
