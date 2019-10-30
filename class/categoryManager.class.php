3fe1a1fca363cd6f11e83e271c3a4a0e5514f8ab
waltercalling <waltercalling@free.fr>
2019-10-30 13:03:05 +0100

    Update categoryManager.class.php

--- class/categoryManager.class.php
+++ class/categoryManager.class.php
@@ -1,1 +1,24 @@
 <?php
+class categoryManager{
+	private $db; // Instance de PDO.
+
+	public function __construct($db) {
+    $this->setDb($db);
+  }
+	public function setDb(PDO $db) {
+    $this->db = $db;
+  }
+
+public function add(Category $category) {
+	  // Prepare category insert
+	  $add_category = $this->db->prepare('INSERT INTO category(type) VALUES(:type)');
+	  $add_category->bindValue(':fabricant', $category->getType(), PDO::PARAM_STR);
+	  // Execute insert category
+	  $add_category->execute();
+	  // Close cursor of the insert category
+	  $add_category->closeCursor();
+	  echo '<p><strong><u>!! Categorie bien ajoutée !!</u></strong></p>';
+	  return $this;
+	}
+
+}
