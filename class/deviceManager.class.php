17afa89dcd2334d41c969a7de6589fb520fdd817
WillySterr <54704862+WillySterr@users.noreply.github.com>
2019-10-30 15:45:31 +0100

    device update

--- class/deviceManager.class.php
+++ class/deviceManager.class.php
@@ -9,21 +9,43 @@
 			$this->_db = $db;
 		}
 
-	public function add(Device $device){
-		//préparation de la requête d'insertion.
-		$add_device = $this->_db->prepare('INSERT INTO device(name) VALUES (:name)');
-		$add_device->bindValue(':name', $device->getName(), PDO::PARAM_STR);
-		// Execution de la requête
-		$add_device->execute();
-		//closeCursor() libère la connexion au serveur, permettant aisni à d'autres requêtes SQL d'être exectuées
-		$add_device->closeCursor();
-		echo '<p>Device Added</p>';
+		public function add(Device $device){
+			//préparation de la requête d'insertion.
+			$add_device = $this->_db->prepare('INSERT INTO device(name) VALUES (:name)');
+			$add_device->bindValue(':name', $device->getName(), PDO::PARAM_STR);
+			// Execution de la requête
+			$add_device->execute();
+			//closeCursor() libère la connexion au serveur, permettant aisni à d'autres requêtes SQL d'être exectuées
+			$add_device->closeCursor();
+			echo '<p>Device Added</p>';
 
-	}
-
-
+		}
 
 
+		public function read(){
+			$read_device = $this->_db->query('SELECT * FROM device');
+			return $read_device->fetchAll(PDO::FETCH_ASSOC);
+		}
 
+		public function readObj(){
+				$devices = [];
+				$read_object = $this->_db->query('SELECT * FROM device');
+				while ($obj = $read_object->fetch(PDO::FETCH_ASSOC)) {
+				    $devices[] = new Device($obj);
+
+				}
+				return $devices;
+			}
+
+		public function del(Device $device){
+		$this->_db->exec('DELETE FROM device WHERE id_device='.$device->getId_device());
+		}
+
+		public function update(Device $device){
+		$new_device = $this->_db->prepare('UPDATE device SET id_device = :id_device, name = :name, WHERE id_device ='.$device->getId_device());
+		$new_device->bindValue(':id_device', $device->getId_device(), PDO::PARAM_INT);
+		$new_device->bindValue(':name', $device->getName(), PDO::PARAM_STR);
+	
+	} 
 
 	}
--- device.php
+++ device.php
@@ -7,11 +7,40 @@
 
 	include_once 'inc/connect.php';
 
-	$device = new Device ([
-		'name' => 'Playstation 4',
+	$device1 = new Device ([
+		'name' => 'Playstation 5 (à venir)',
 	]);
 
 
 	$manager = new DeviceManager($bdd);
 
+	//$manager->add($device1);
+
+	$devices = $manager->readObj();
+
+
+
+	//$devices = $manager->read();
+
+	//$manager->del($devices[9]);
+
+
+
+
+
+	echo "<table>";
+	echo "<thead>";
+	echo "<th> Type de support </th>";
+	echo "<th> Modifier nom </th>";
+	echo "<th> Supprimer support </th>";
+	echo "<thead>";
+	echo "<tbody>";
+
+	foreach ($devices as $key => $value) {
+
+		echo "<tr><td>".$value->getName()."</td><td>[...]</td><td>[...]</td></tr>";
+	}
+	
+	echo "<tbody>";
+	echo "</table>";
 ?>
