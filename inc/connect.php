439f5bfca93762036a50ea8adf9fc4426f008c34
jscowbf3 <jscowbf3@gmail.com>
2019-10-30 11:44:40 +0100

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,12 +1,9 @@
 <?php
 // Database connexion
-$bdd = new PDO('mysql:host=localhost;dbname=DATABASENAME;charset=utf8', 'root', '', [PDO::ATTR_EMULATE_PREPARES=>false]);
-// host=server, dbname=database name, charset=encoding type, root'= user name, ''= password
-
 // Check of the database connexion
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=DATABASENAME;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=jvcom;charset=utf8', 'root', '', [PDO::ATTR_EMULATE_PREPARES=>false]);
 	echo'Database is connected';
 }
 catch (Exception $e)
