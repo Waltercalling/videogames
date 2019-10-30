d2cd6fe375ee39fe33c82107436abea8230196f2
WillySterr <54704862+WillySterr@users.noreply.github.com>
2019-10-31 00:15:59 +0100

    update pas fini

--- class/deviceManager.class.php
+++ class/deviceManager.class.php
@@ -42,14 +42,22 @@
 		$this->_db->exec('DELETE FROM device WHERE id_device='.$device->getId_device());
 		}
 
-		public function update(Device $device){
+/*		public function update(Device $device){
 		$new_device = $this->_db->prepare('UPDATE device SET id_device = :id_device, name = :name, WHERE id_device ='.$device->getId_device());
 		$new_device->bindValue(':id_device', $device->getId_device(), PDO::PARAM_INT);
 		$new_device->bindValue(':name', $device->getName(), PDO::PARAM_STR);
 	
-		}
+		}*/
 
 		public function delById($id){
 		$this->_db->exec('DELETE FROM device WHERE id_device ='.$id); 
+		}
+
+			public function updateById($id){
+		$new_device = $this->_db->prepare('UPDATE device SET /*id_device = :id_device,*/ name = :name, WHERE id_device ='.$id);
+/*		$new_device->bindValue(':id_device', $device->getId_device(), PDO::PARAM_INT);
+*/		$new_device->bindValue(':name', $id->getName(), PDO::PARAM_STR);
+		$new_device->execute();
+	
 		}
 	}
---
+++ deviceUpdate.php
@@ -0,0 +1,62 @@
+<?php spl_autoload_register(function($class){
+	require_once'class/'.$class.'.class.php'; }); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<!-- Head load -->
+<?php include("inc/head.php"); ?>
+<title>Ajouter une catégorie</title>
+</head>
+<!-- Database connexion -->
+<?php include_once("inc/connect.php");?>
+
+<body>
+	<!-- Header -->
+	<?php include("inc/header.php"); ?>
+	<main>
+
+	<h1 class="text-center my-3">Modifier un support</h1>
+
+<section class="w-50 m-auto">
+	
+	<form class="border border-dark rounded bg-light p-5" action="" method="POST">
+		
+		<label for="updName">Nouveau Nom</label>
+		<?php 		
+		$manager = new DeviceManager($bdd);
+		$id = $_GET['id'];
+	//	$devices = $manager->readObj();
+
+?>
+		<input type="text" name="updName" class="form-control" placeholder="[attends l'encien nom]" />
+
+		<div class="row">
+			<!-- Cancel Button -->
+			<div class="col-12 col-lg-4 mb-2 my-2">
+				<a title="Annuler" href="device.php" type="button" name="back" class="btn btn-danger shadow-sm border border-dark w-100"/>Annuler</a>
+			</div>
+			<!-- Validate Button -->
+			<div class="col-12 col-lg-6 offset-lg-2 my-2">
+				<input type="submit" name="updSup" value="Modifier le support" class="btn btn-success shadow-sm border border-dark w-100" title="Enregistrer le support"/>
+			</div>	
+		</div>
+
+	</form>
+
+	<?php
+	if (isset($_POST['updName']) && !empty($_POST['updName'])){
+		$manager = new DeviceManager($bdd);
+		$devices = new Device (['name' => $_POST['updName']]);
+		$manager->updateById($id);
+	}else{
+		echo'formulaire vide';
+	}
+
+	?>
+
+</section>
+	<!-- Footer -->
+	<?php include("inc/footer.php"); ?>
+
+</body>
+</html>
