074181e5019f5de4cd961c79a77a0a57e387358f
Waltercalling <waltercalling@free.fr>
2019-10-31 07:37:17 +0100

    Update list-studio.php

--- list-studio.php
+++ list-studio.php
@@ -17,32 +17,36 @@
 ?>
 
 <body>
-    <table class="table">
-        <thead>
-                <tr>
-                    <th scope="col">Nom</th>
-                    <th scope="col">Lien</th>
-                    <th scope="col">Supprimer</th>
-                    <th scope="col">Modifier</th>
-                
-                </tr>       
-        </thead>
-        <tbody>
-            
-            <?php 
-                foreach($tab_list as $key => $value):
-            ?> 
-            <tr>
-                    <td><?= $value->getName(); ?></td>
-                    <td><?= $value->getId_studio(); ?></td>
-                    <td><?= $value->getLink(); ?></td>
-                    <td><a class="btn btn-danger" href="deleteStudio.php?id=<?= $value->getId_studio();?>">Supprimer</a></td>
-                    <td><a class="btn btn-warning" href="upgradeStudio.php?id=<?= $value->getId_studio();?>">Modifier</a></td>
-            </tr>
-                <?php endforeach;?>
-            
-           
-        </tbody>
-    </table>
+    <h1 class="text-center my-3">Liste des éditeurs</h1>
+    <section class="border border-dark w-50 m-auto rounded shadow">
+        <table class="m-auto table table-striped table-hover">
+            <thead class="thead-dark text-white font-weight-bold">
+                    <tr>
+                        <th>Id</th>
+                        <th scope="col" class="w-25">Nom</th>
+                        <th scope="col" class="w-50">Lien</th>
+                        <th scope="col" class="">Supprimer</th>
+                        <th scope="col" class="">Modifier</th>
+                    
+                    </tr>       
+            </thead>
+             <tbody>
+                    <?php foreach($tab_list as $key => $value): ?> 
+                    <tr>
+                        <td><?= $value->getId_studio(); ?></td>
+                        <td><?= $value->getName(); ?></td>
+                        <td><?= $value->getLink(); ?></td>
+                        <td><a class="cancel" href="deleteStudio.php?id=<?= $value->getId_studio();?>"><i class="delete fas fa-trash-alt" title="Supprimer"></i></a></td>
+                        <td><a class="" href="upgradeStudio.php?id=<?= $value->getId_studio();?>"><i class="fas fa-edit pr-2" title="Modifier"></i></a></td>
+
+                        <div class="d-flex flex-lg-row justify-content-lg-around">
+                        <div><a class="cancel" href="update-category.php?id=<?= $value['id_category'];?>"><i class="fas fa-edit pr-2" title="Modifier"></i></a></div>
+                        <div><a href="delete-category.php?id=<?= $value['id_category'];?>"><i class="delete fas fa-trash-alt" title="Supprimer"></i></a></div>
+                    </div>
+                    </tr>
+                        <?php endforeach;?>
+            </tbody>
+        </table>  
+    </section>   
 </body>
 <?php require_once 'inc/footer.php'; ?>
