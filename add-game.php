42634e3d6f701b1c6856b8bbd6c769065c35209c
jscowbf3 <jscowbf3@gmail.com>
2019-10-31 11:29:11 +0100

    Update add-game.php

--- add-game.php
+++ add-game.php
@@ -38,9 +38,9 @@
         <?php
         // récupérer la liste des catégories et la liste des editeurs, et les mettre dans un select
         
-        // je rapatrie ici un TABLEAU ASSOCIATIF (méthode listcategory)
+        // je rapatrie ici un TABLEAU ASSOCIATIF D'OBJETS (méthode getObjCategory())
         $cm = new categoryManager($bdd);
-        $listCat = $cm->listCategory();
+        $listCat = $cm->getObjCategory();
         // echo"<pre>";
         // print_r($listCat);
         // echo"</pre>";
@@ -60,17 +60,17 @@
         <select name="category" id="category" class="form-control">
             <option value="" selected>--Catégorie--</option>
             <?php
-            foreach ($listCat as $idList => $catTab){
-                echo '<option value="'.$catTab['id_category'].'">'.$catTab['type'].'</option>';
+            foreach ($listCat as $idList => $catObj){
+                echo '<option value="'.$catObj->getId_category().'">'.$catObj->getType().'</option>';
             }
             ?>
             
             
         </select>
         <br/>
-        <label for="editor">Choisissez l'éditeur</label>
+        <label for="studio">Choisissez l'éditeur</label>
 
-        <select name="editor" id="editor" class="form-control">
+        <select name="studio" id="studio" class="form-control">
             <option value="" selected>--Editeur--</option>
             <?php
             foreach ($listEd as $idList => $EdObj){
@@ -88,7 +88,7 @@
 			</div>
 			<!-- Validate Button -->
 			<div class="col-12 col-lg-6 offset-lg-2 my-2">
-				<input type="submit" name="sendCat" value="Enregistrer la catégorie" class="btn btn-success shadow-sm border border-dark w-100" title="Enregistrer le jeu"/>
+				<input type="submit" name="sendCat" value="Enregistrer le jeu" class="btn btn-success shadow-sm border border-dark w-100" title="Enregistrer le jeu"/>
 			</div>	
 		</div>
 
@@ -101,7 +101,7 @@ if (isset($_POST['title']) && !empty($_POST['title'])){
 
 
     $gm = new GameManager($bdd);
-    $gameIn = ['title'=>$_POST['title'],'description'=>$_POST['description'],'pegi'=>$_POST['pegi'],'link'=>$_POST['link'],'id_category'=>1,'id_studio'=>1];
+    $gameIn = ['title'=>$_POST['title'],'description'=>$_POST['description'],'pegi'=>$_POST['pegi'],'link'=>$_POST['link'],'id_category'=>$_POST['category'],'id_studio'=>$_POST['studio']];
     // echo"<pre>";
     // print_r($gameIn);
     // echo"</pre>";
